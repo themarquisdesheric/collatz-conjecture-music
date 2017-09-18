c8472fb39ef9b26c181af9b98869ca5976a41229
Yuval Allweil <themarquisdesheric@gmail.com>
2017-09-18 15:01:17 -0700

    created List & ListItem components, minor styling

--- app/src/components/List.js
+++ app/src/components/List.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 export default function List({ sequence = [] }) {
   return (
-    <ul>
-      {sequence.map(function(num, i) {
-        return (num % 2 === 0)
-          ? <li key={i}>{num} is even so we divide by 2</li>
-          : <li key={i}>{num} is odd so we multiply by 3, then add 1</li>;
-      })}
-    </ul>
+    <div>
+      <ul style={{
+        listStyleType: 'none',
+        paddingLeft: 0
+      }}>
+        {sequence.map(function(num, i) {
+          if (i === sequence.length - 1) return num;
+
+          return (num % 2 === 0)
+            ? <li key={i}>{num} is even so we divide by 2</li>
+            : <li key={i}>{num} is odd so we multiply by 3, then add 1</li>;
+        })}
+      </ul>
+    </div>
   );
 }
