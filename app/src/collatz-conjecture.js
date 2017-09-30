fb6cc0813e25dafc80de1ea73f8276706c9e109a
Yuval Allweil <themarquisdesheric@gmail.com>
2017-09-30 13:14:01 -0700

    switched to logarithmic step for semitones so it would sound more like an instrument, but not sure if I like it. Linear step seems to make the pattern more apparent for the final stages

--- app/src/collatz-conjecture.js
+++ app/src/collatz-conjecture.js
@@ -29,15 +29,20 @@ function playCollatz(sequence) {
   osc.type = 'sine';
   osc.connect(audioCtx.destination);
   osc.start();
+  osc.frequency.value = 0;
   osc.stop(sequence.length);
 
   var intervalID = setInterval(function() {
-    if (counter === sequence.length - 1) {
+    if (counter === sequence.length) {
       clearInterval(intervalID);
       osc.disconnect(audioCtx.destination);
     } else {
-      // multiplying frequency by 110 to hear better, and by 1.05946 for the semitones step
-      osc.frequency.value = sequence[counter] * 110 * 1.05946;
+      // logarithmic step for the semitones, like a fretboard
+      var note = 880 * Math.pow(1.05946, sequence[counter] - 1);
+      osc.frequency.value = note;
+
+      // arbitrary linear step: multiplying frequency by 110 for floor to be within hearing range, and by 1.05946 for the semitones step
+      // osc.frequency.value = sequence[counter] * 110 * 1.05946;
       counter++;
     }
   }, 300);
