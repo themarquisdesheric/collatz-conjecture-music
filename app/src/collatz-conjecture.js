0c3c84cb4b604df7f74fa8e70a7464db6ab578e1
Yuval Allweil <themarquisdesheric@gmail.com>
2017-10-06 20:26:55 -0700

    changed to inner function, removed linear step code

--- app/src/collatz-conjecture.js
+++ app/src/collatz-conjecture.js
@@ -1,6 +1,31 @@
 function collatz(num) {
   var sequence = [num];
 
+  function playCollatz(sequence) {
+    var AudioContext = window.AudioContext || window.webkitAudioContext;
+    var audioCtx = new AudioContext();
+    var osc = audioCtx.createOscillator();
+    var counter = 0;
+  
+    osc.type = 'sine';
+    osc.connect(audioCtx.destination);
+    osc.start();
+    osc.frequency.value = 0;
+    osc.stop(sequence.length);
+  
+    var intervalID = setInterval(function() {
+      if (counter === sequence.length) {
+        clearInterval(intervalID);
+        osc.disconnect(audioCtx.destination);
+      } else {
+        // logarithmic step for the semitones, like a fretboard
+        osc.frequency.value = 880 * Math.pow(1.05946, sequence[counter] - 1);
+
+        counter++;
+      }
+    }, 300);
+  }
+
   while (num > 1) {
     if (num % 2 === 0) {
       console.log(num + ' is even, so we divide it by 2.');
@@ -18,32 +43,4 @@ function collatz(num) {
   console.log(num);
 
   playCollatz(sequence);
-}
-
-function playCollatz(sequence) {
-  var AudioContext = window.AudioContext || window.webkitAudioContext;
-  var audioCtx = new AudioContext();
-  var osc = audioCtx.createOscillator();
-  var counter = 0;
-
-  osc.type = 'sine';
-  osc.connect(audioCtx.destination);
-  osc.start();
-  osc.frequency.value = 0;
-  osc.stop(sequence.length);
-
-  var intervalID = setInterval(function() {
-    if (counter === sequence.length) {
-      clearInterval(intervalID);
-      osc.disconnect(audioCtx.destination);
-    } else {
-      // logarithmic step for the semitones, like a fretboard
-      var note = 880 * Math.pow(1.05946, sequence[counter] - 1);
-      osc.frequency.value = note;
-
-      // arbitrary linear step: multiplying frequency by 110 for floor to be within hearing range, and by 1.05946 for the semitones step
-      // osc.frequency.value = sequence[counter] * 110 * 1.05946;
-      counter++;
-    }
-  }, 300);
 }
