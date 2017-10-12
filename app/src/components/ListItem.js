d795f40ea0640a50c70becd6ddcdc6a2ef0ffaaf
Yuval Allweil <themarquisdesheric@gmail.com>
2017-10-11 22:14:39 -0700

    you can hear the tones on mouseover, but they sound crazy. Not sure if they're beautiful or terrible

--- app/src/components/ListItem.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ListItem = ({ num, i }) => (num % 2 === 0)
-  ? <li key={i}>{num} is even so we divide by 2</li>
-  : <li key={i}>{num} is odd so we multiply by 3, then add 1</li>;
-
-export default ListItem;
