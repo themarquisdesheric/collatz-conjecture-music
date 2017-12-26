bc59b67d4116ba22b699720da2e9e91344ed9083
Yuval Allweil <themarquisdesheric@gmail.com>
2017-12-25 20:59:04 -0800

    installed airbnb eslint rules, removed errata

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReallySmoothScroll from 'really-smooth-scroll';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+ReallySmoothScroll.shim();
 
 ReactDOM.render(
   <App />,
