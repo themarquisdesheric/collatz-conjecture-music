7bb66eb7cdae45aa7e27f05daa0bfff23fc6320e
Yuval Allweil <themarquisdesheric@gmail.com>
2019-04-06 13:12:25 -0700

    Updates

---
+++ precache-manifest.58f4d1363fe2aaba0ac94f559daa5e34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "867507c3fa10bf32a72a",
+    "url": "/collatz-conjecture-music/static/js/runtime~main.0d63de3e.js"
+  },
+  {
+    "revision": "282a12774bc2552cca38",
+    "url": "/collatz-conjecture-music/static/js/main.cb8540f7.chunk.js"
+  },
+  {
+    "revision": "f7678af4ffd24e9260e3",
+    "url": "/collatz-conjecture-music/static/js/2.43e5e793.chunk.js"
+  },
+  {
+    "revision": "282a12774bc2552cca38",
+    "url": "/collatz-conjecture-music/static/css/main.d0938678.chunk.css"
+  },
+  {
+    "revision": "47e2fb73e3b910334ca2b2c350f825f3",
+    "url": "/collatz-conjecture-music/index.html"
+  }
+];
